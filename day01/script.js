4cf61c0dce1a9e67dd50c515ab90785368c816a8
leejidev <leejidev@gmail.com>
2026-08-21 15:49:12 +0900

    feat: 영화 데이터 생성 및 콘솔 출력 구현

---
+++ day01/script.js
@@ -0,0 +1,15 @@
+// 1. 영화 데이터 변수 만들기
+const title = "인터스텔라";
+const voteAverage = 8.7;
+const voteCount = 32000;
+const popularity = 150.5;
+const releaseDate = "2014-11-05";
+const originalLanguage = "en";
+
+// 2. 영화 데이터 Console에 출력하기
+console.log(title);
+console.log(voteAverage);
+console.log(voteCount);
+console.log(popularity);
+console.log(releaseDate);
+console.log(originalLanguage);
