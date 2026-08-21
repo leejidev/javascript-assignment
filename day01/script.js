9121289d8a1c4aadc71380be3f9a6d91c5758b6e
leejidev <leejidev@gmail.com>
2026-08-21 15:51:13 +0900

    feat: 문자열 연결과 Template Literal로 영화 정보 출력

--- day01/script.js
+++ day01/script.js
@@ -24,3 +24,13 @@ const genre = "SF";
 const category = "영화";
 
 console.log(genre + " " + category);
+
+// 5. 영화 정보를 문자열로 조합하기
+console.log("영화 제목: " + title);
+console.log("평점: " + voteAverage);
+console.log("개봉일: " + releaseDate);
+
+// 6. Template Literal 사용하기
+console.log(`영화 제목: ${title}`);
+console.log(`평점: ${voteAverage}`);
+console.log(`개봉일: ${releaseDate}`);
