b9422109c91a637d4d8793f306c768c93dde9f29
leejidev <leejidev@gmail.com>
2026-08-24 15:42:37 +0900

    feat: 영화 객체 생성 및 Property 출력 구현

---
+++ day02/script.js
@@ -0,0 +1,13 @@
+// 1. 영화 한 편의 데이터를 객체로 만들기
+const movie = {
+  id: 1,
+  title: "인셉션",
+  voteAverage: 8.4,
+  voteCount: 35000,
+  releaseDate: "2010-07-15",
+  isFavorite: false,
+};
+
+console.log(movie.title);
+console.log(movie.voteAverage);
+console.log(movie.isFavorite);
