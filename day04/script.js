91a0da82975cb86319c7d540e5c7123b723474d0
leejidev <leejidev@gmail.com>
2026-08-26 17:36:49 +0900

    feat: Form 제출 및 검색어 입력 처리

--- day04/script.js
+++ day04/script.js
@@ -5,3 +5,24 @@ const recommendResult = document.querySelector("#recommend-result");
 recommendButton.addEventListener("click", () => {
   recommendResult.textContent = "오늘의 추천 영화는 인셉션입니다.";
 });
+
+// 2. Form 제출 처리하기
+const searchForm = document.querySelector("#search-form");
+const searchInput = document.querySelector("#search-input");
+const searchResult = document.querySelector("#search-result");
+
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // 3. 빈 검색어 처리하기
+  const keyword = searchInput.value.trim();
+
+  if (keyword) {
+    searchResult.textContent = `검색한 영화: ${keyword}`;
+  } else {
+    searchResult.textContent = "";
+  }
+
+  // 4. 검색 후 Input 비우기
+  searchInput.value = "";
+});
