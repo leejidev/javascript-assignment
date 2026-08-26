34754c2a32a92a89bebfcd546b21b079190c3cbe
leejidev <leejidev@gmail.com>
2026-08-26 17:37:35 +0900

    feat: forEach로 영화 목록 순회 및 화면 출력

--- day04/script.js
+++ day04/script.js
@@ -26,3 +26,31 @@ searchForm.addEventListener("submit", (event) => {
   // 4. 검색 후 Input 비우기
   searchInput.value = "";
 });
+
+// 5. forEach()로 영화 목록 출력하기
+const movies = [
+  {
+    title: "인셉션",
+    voteAverage: 8.4,
+  },
+  {
+    title: "인터스텔라",
+    voteAverage: 8.7,
+  },
+  {
+    title: "다크 나이트",
+    voteAverage: 9.0,
+  },
+];
+movies.forEach((movie) => {
+  console.log(`${movie.title}의 평점은 ${movie.voteAverage}입니다.`);
+});
+
+// 6. 영화 목록을 화면에 출력하기
+const movieList = document.querySelector("#movie-list");
+
+movies.forEach((movie) => {
+  const li = document.createElement("li");
+  li.textContent = movie.title;
+  movieList.append(li);
+});
